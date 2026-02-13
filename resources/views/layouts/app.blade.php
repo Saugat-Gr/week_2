783d02850d4e5763f4ee466d89e87bef3992536f
Saugat Gurung <gganosh9@gmail.com>
2026-02-13 15:26:13 +0545

    latest pull

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title', 'Demo')</title>
 
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}"> 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
 </head>
